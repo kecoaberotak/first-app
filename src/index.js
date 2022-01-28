24e4ba878c800fcccb11d18eb88afe8f75ac100c
KecoaBerotak <dennisfernandes0212@gmail.com>
2022-01-28 13:35:45 +0700

    index.js catatan inline style dan penulisan html di JSX

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
 
+
 // ini pakai arrow function
 const App = () => {
+	// di jsx bisa memanggil apa saja, bahkan object
+	const buttonText = {text: 'submit'};
+	const style = {backgroundColor: 'red', color: 'white'};
+
 	return(
-		<div>Hi There!</div>
+		<div>
+			{/* pada JSX, class diganti className */}
+			<label htmlFor="name" className="label">enter e-mail</label>
+			<input id="name" type="text"/>
+
+			{/* pakai dua untuk menerapkan inline style, pakai dua kurung kurawal */}
+			{/* kurung paling luar untuk menandakan penulisan nilai dalam JSX */}
+			{/* dan yg didalam untuk menandakan javascript object */}
+			{/* penulisan menggunakan camelCase */}
+			{/* double quote untuk menandakan string, dan sisanya pakai single quote */}
+			<button style={{backgroundColor: 'red', color: 'white'}}>{buttonText.text}</button>
+
+			{/* bisa juga seperti ini untuk inline style*/}
+			<button style={style}>{buttonText.text}</button>
+		</div>
 	)
 }
+
+
 
 // ini pakai function biasa
 // function App() {
@@ -14,6 +35,8 @@ const App = () => {
 // 		<div>Hi There!</div>
 // 	)
 // }
+
+
 
 // Dua cara render ini sama aja
 // ReactDOM.render(<App/>, document.getElementById('root'))
